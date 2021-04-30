e2a793769e5dfa7d656b5f09e04e364ce957f58d
PVkolos <70021890+PVkolos@users.noreply.github.com>
2021-04-30 14:33:14 +0300

    relese version 6

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from tkinter import *
 import tkinter
 from tkinter import messagebox as mbox
-import tkcap
 import os
 from tkinter import colorchooser
 from tkinter.ttk import *
+import PIL
+from PIL import Image, ImageDraw
 
 root = Tk()
 root.geometry("1200x800")
@@ -15,41 +16,67 @@ canvas = Canvas(root, bg='white', height=screen_height, width=screen_width)
 root.title('Paint')
 hx = None
 value_color = None
+rgb = None
+col = (255, 255, 255)
 
 
 def draw(event):
-    global value_color
     value = variable.get()
-    value_color = hx
-    if value and value_color:
-        canvas.create_oval((event.x - int(value) // 2, event.y - int(value) // 2), (event.x + int(value) // 2, event.y + int(value) // 2), fill=value_color, outline=value_color)
+    if value and hx:
+        canvas.create_oval((event.x - int(value) // 2, event.y - int(value) // 2),
+                           (event.x + int(value) // 2, event.y + int(value) // 2), fill=hx, outline=hx)
+
+        drawer.ellipse((event.x - int(value) // 2, event.y - int(value) // 2,
+                  event.x + int(value) // 2, event.y + int(value) // 2), hx)
 
 
 def dalate():
+    global image, drawer
+    pixels = image.load()
+    x, y = image.size
+    for i in range(x):
+        for j in range(y):
+            pixels[i, j] = (255, 255, 255)
     canvas.config(bg='white')
     canvas.delete("all")
 
 
 def zal():
+    global image, col, drawer
     if hx:
         canvas.config(bg=hx)
+        im = PIL.Image.new('RGB', (screen_width, screen_height), color=hx)
+        pixels = image.load()
+        pixels2 = im.load()
+        x, y = im.size
+        for i in range(x):
+            for j in range(y):
+                r1, g1, b1 = pixels[i, j]
+                if (r1, g1, b1) != col:
+                    pixels2[i, j] = int(r1), int(g1), int(b1)
+        image = im
+        drawer = ImageDraw.Draw(image)
+        del im
+        col = (int(rgb[0]), int(rgb[1]), int(rgb[2]))
     else:
         mbox.showerror("Ошибка", "Выберите цвет заливки!")
 
 
 def save():
     if not os.path.exists(f'{text.get()}.jpg'):
-        cap = tkcap.CAP(root)
-        cap.capture(f'{text.get()}.jpg')
+        image.save(f'{text.get()}.png')
     else:
         mbox.showerror("Ошибка", "Файл с таким именем существует!")
 
 
 def color():
-    global hx
+    global hx, rgb
     (rgb, hx) = colorchooser.askcolor()
     cn.config(bg=hx)
 
+
+image = PIL.Image.new('RGB', (screen_width, screen_height), 'white')
+drawer = ImageDraw.Draw(image)
 
 variable = StringVar(root)
 shr = OptionMenu(root, variable, 15, 1, 5, 10, 15, 20, 25, 30, 35, 40, 50, 60, 70, 80, 90, 100)
